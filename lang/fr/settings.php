8d2a8462350575fd147971b0f1d44e9e55a4111f
Dominik Eckelmann <deckelmann@gmail.com>
2010-06-30 12:52:57 +0200

    added french translation by Etienne M.

---
+++ lang/fr/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['theme']     = 'Th&egrave;me des ic&ocirc;nes';
+$lang['color']     = 'Couleur du texte (HTML hex)';
+$lang['maxshowen'] = 'Nombre maximum de pages &agrave; afficher sur le r&eacute;sum&eacute; de qualit&eacute;';

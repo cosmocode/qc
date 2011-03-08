cdd976fcaf9a74f4371fcbec3dd0edc08a4aba3c
Sven Lueckenbach <host@wbbcoder.de>
2011-03-08 05:28:24 -0800

    fixed a typo

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -21,7 +21,7 @@ $lang['anonymous']     = 'Anonymous';
 $lang['i_qcscore']     = 'Quality:';
 
 $lang['errorsfound_h'] = 'Possible Quality Problems Identified';
-$lang['errorsfound']   = 'After analyzing the page structure, a few possible problems about the readability and usability of the page where identified. Please have a look at the points below and see if you can correct them. Keep in mind that this was an automatic analysis - you need to judge yourself if it is correct.';
+$lang['errorsfound']   = 'After analyzing the page structure, a few possible problems about the readability and usability of the page were identified. Please have a look at the points below and see if you can correct them. Keep in mind that this was an automatic analysis - you need to judge yourself if it is correct.';
 
 $lang['fixme_h']       = '%d FIXME(s)';
 $lang['fixme']         = 'The page contains markers about missing or incorrect content. You should replace the FIXME markers with corrected content.';

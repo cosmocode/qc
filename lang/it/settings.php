1ab10a53b99dce72d97d086bcb442591cf8bc881
Edmondo Di Tucci <snarchio@gmail.com>
2013-08-13 20:11:30 +0200

    translation update

---
+++ lang/it/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Edmondo Di Tucci <snarchio@gmail.com>
+ */
+$lang['adminonly']             = 'Il Quality Summary è visibile solo agli utenti del gruppo amministratori';
+$lang['single_author_only']    = 'C\'è uno solo o comunque pochi autori';

c804e2c97f908df0321170f5c59679d101534655
Edmondo Di Tucci <snarchio@gmail.com>
2013-08-15 11:45:39 +0200

    translation update

--- lang/it/lang.php
+++ lang/it/lang.php
@@ -7,6 +7,7 @@
  */
 $lang['menu']                  = 'Quality Summary';
 $lang['admin_headline']        = 'Quality Summary';
+$lang['admin_desc']            = 'Qui sono elencate le %d pagine con il maggior numero di FIXME e il peggior indice di qualità. Clicca sul titolo di una cella per ordinare. Il sommario è aggiornato giornalmente.';
 $lang['admin_page']            = 'Pagina';
 $lang['admin_quality']         = 'Qualità';
 $lang['admin_fixme']           = 'Correggimi';
@@ -20,11 +21,16 @@ $lang['g_authors']             = 'Maggiori Autori:';
 $lang['anonymous']             = 'Anonimo';
 $lang['i_qcscore']             = 'Qualità:';
 $lang['errorsfound_h']         = 'Possibili problemi di qualità identificati';
+$lang['errorsfound']           = 'Dopo aver analizzato la struttura della pagina sono stati identificati alcuni problemi sulla leggibilità e l\'usabilità della pagina. Per piacere, dai un\'occhiata ai punti qui sotto e guarda se puoi correggerli. Ricorda che questa è un\'analisi automatica; devi giudicare tu se è corretta.';
 $lang['fixme_h']               = '%d CORREGGIMI';
+$lang['fixme']                 = 'La pagina contiene marcatori riguardanti contenuti mancanti o incorretti. Devi sostituire i FIXME con i contenuti corretti.';
 $lang['noh1_h']                = 'Non c\'è un titolo';
+$lang['noh1']                  = 'Una pagina deve sempre cominciare con un titolo di livello 1. Questo titolo è considerato la descrizione del contenuto della pagina.';
 $lang['manyh1_h']              = '%d titoli';
+$lang['manyh1']                = 'La pagina contiene più titoli di livello 1. Una pagina deve contenere un solo un titolo di livello 1 che rappresenti il contenuto principale della pagina. Se la tua pagina contiene più contenuti principali prendi in considerazione di dividerla in più pagine.';
 $lang['headernest_h']          = 'Sezioni innestate in modo errato';
 $lang['manyhr_h']              = 'Molte righe orizzontali';
+$lang['manyhr']                = 'La pagina contiene più linee orizzontali (<code>----</code>). Le linee orizzontali devono essere usare molto raramente perché peggiorano la leggibilità e la struttura di un documento. Prendi in considerazione di ristrutturare la pagina usando sezioni e paragrafi.';
 $lang['manybr_h']              = 'Troppi a capo forzati';
 $lang['deepquote_h']           = 'Virgolette troppo innestate';
 $lang['singleauthor_h']        = 'Un solo autore';

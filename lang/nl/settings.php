ff8fd9668c022c1b2438d7060b5f92cb342f8ff3
Rene <wllywlnt@yahoo.com>
2014-03-20 09:00:06 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['theme']                 = 'Het te gebruiken thema in de kwaliteis regels.';
+$lang['color']                 = 'Tekstkleur te gebruiken in de voorbeeld weergave (HTML hex)';
+$lang['maxshowen']             = 'Maximum aantal getoonde pagina\'s in de Kwaliteitssamenvatting';
+$lang['adminonly']             = 'Kwaliteissamenvatting alleen zichtbaar voor leden van de admin groep.';
+$lang['single_author_only']    = 'Er is slechts een of enkele auteurs.';

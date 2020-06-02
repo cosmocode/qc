0572c387008837a29840c6292111f83ea26988e0
Wes from StarArmy.com <stararmy@gmail.com>
2020-06-02 15:38:59 -0400

    Update renderer.php
    File moved in Hogfather from Parser to Parsing

--- renderer.php
+++ renderer.php
@@ -2,7 +2,7 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
-require_once DOKU_INC.'inc/parser/renderer.php';
+require_once DOKU_INC.'inc/Parsing/renderer.php';
 require_once DOKU_INC.'inc/fulltext.php';
 
 /**

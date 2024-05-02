aa3656605d6e8c0e50cb212b8030ba4da32e8237
Zweihorn <4863737+Zweihorn@users.noreply.github.com>
2024-05-02 19:53:18 +0200

    new conf option 'ignore_header_count'
    - provides new conf option 'ignore_header_count'
    - applies this option in `renderer.php`

--- conf/metadata.php
+++ conf/metadata.php
@@ -4,3 +4,4 @@ $meta['maxshowen'] = array('numeric');
 $meta['adminonly'] = array('onoff');
 $meta['single_author_only'] = array('onoff');
 $meta['ignore_nobacklink'] = array('onoff');
+$meta['ignore_header_count'] = array('onoff');
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -4,3 +4,4 @@ $lang['maxshowen'] = 'Maximum displayed pages on Quality Summary';
 $lang['adminonly'] = 'Quality Summary only visible for members of the admin group';
 $lang['single_author_only'] = 'There is only one or a few authors.';
 $lang['ignore_nobacklink'] = 'Ignore the scoring of missing backlinks. This might be applicable when the Alternative Breadcrumbs feature is providing a backlink each for all pages e.g., by the sprintdoc template'
+$lang['ignore_header_count'] = 'Ignore the scoring of more than one H1 header.'
